f1e49d796596cee6806b61bd73acfb3fcd160b74
NaoufalELMESKINI <naoufal.elmeskini@gmail.com>
2025-10-01 14:30:43 +0200

    click folds

--- src/Rule.jsx
+++ src/Rule.jsx
@@ -3,18 +3,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faChevronDown, faPencil, faThumbsDown, faThumbsUp,
 } from '@fortawesome/free-solid-svg-icons';
+import {useState} from "react";
+import {faChevronUp} from "@fortawesome/free-solid-svg-icons/faChevronUp";
 
 /**
  * Display a single rule.
  */
 function Rule({ rule }) {
+    const [folded , setFolded] = useState(false);
+    const clickHAndler = () => {
+        setFolded(!folded)
+    }
+
     return (
         <section className="m-16 mt-4 cursor-pointer">
-            <header className="bg-blue-400 text-3xl p-4 text-white flex justify-between items-center">
-                {rule.title} <FontAwesomeIcon icon={faChevronDown} className="m-4" />
+            <header className="bg-blue-400 text-3xl p-4 text-white flex justify-between items-center" onClick={clickHAndler}>
+                {rule.title} <FontAwesomeIcon icon={folded ? faChevronUp: faChevronDown} className="m-4" />
             </header>
-            <p className="p-4 text-2xl">{rule.description}</p>
-            <footer className="flex justify-between items-center p-4 border">
+            <p className="p-4 text-2xl" hidden = {folded} >{rule.description}</p>
+            <footer className="flex justify-between items-center p-4 border" hidden = {folded} >
                 <div>
                     {rule.tags.map(
                         (tag) => <span key={tag} className="p-4 bg-gray-400 rounded text-white text-2xl mx-4">{tag}</span>,
