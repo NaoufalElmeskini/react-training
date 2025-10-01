756c628dcae24328ae9cdd2bccac702ea2ea9f6f
NaoufalELMESKINI <naoufal.elmeskini@gmail.com>
2025-10-01 16:35:30 +0200

    ajouter hook : useEffect (//= (ng) ngOnInit, (vuejs) mount)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 import {RuleList} from "./RuleList.jsx";
-import data from "./data.json";
+import {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSpinner} from "@fortawesome/free-solid-svg-icons/faSpinner";
 
 function App() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = () => fetch('/data.json')
+            .then((res) => res.json())
+            .then(d => setData(d))
+        setTimeout(fetchData, 3000)
+    }, [data]);
+
     return (
-        <RuleList rules={data}/>
+        data.length === 0 ?
+            <FontAwesomeIcon icon={faSpinner} /> :
+            <RuleList rules={data}/>
     );
 }
 
