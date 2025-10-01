fe26ad066e33d5b8ad4d4be90cabce67776f7052
NaoufalELMESKINI <naoufal.elmeskini@gmail.com>
2025-10-01 12:10:20 +0200

    ajouter tailwind + rules

---
+++ src/xx.jsx
@@ -0,0 +1,27 @@
+export const rule = () =>
+    <section className="m-16 mt-4">
+        <header className="bg-blue-400 text-3xl p-4 text-white flex justify-between items-center">
+            Leave the code cleaner than you found it. <i className="fa-chevron-down m-4"/>
+        </header>
+        <p className="p-4 text-2xl">From Clean Code: always leave the code cleaner than it was before.</p>
+        <footer className="p-4 border flex justify-between items-center">
+            <div>
+                <span className="p-4 bg-gray-400 rounded text-white text-2xl mx-4">craftsmanship</span>
+                <span className="p-4 bg-gray-400 rounded text-white text-2xl mx-4">clean code</span>
+            </div>
+            <div className="flex justify-end">
+                <button type="button" className="bg-blue-400 p-4 rounded text-2xl">
+                    <i className="fa-pencil"></i>
+                </button>
+                <div className="flex">
+                    <button type="button" className="border p-4" title="+1">
+                        0 <i className="fa-thumbs-up"></i>
+                    </button>
+                    <button type="button" className="border p-4" title="-1">
+                        0 <i className="fa-thumbs-down"></i>
+                    </button>
+                </div>
+            </div>
+        </footer>
+    </section>
+// createRoot('').render(rule())
