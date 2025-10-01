1a3f7cbe2cf872e6359f5f1b0fd578e68a8261be
NaoufalELMESKINI <naoufal.elmeskini@gmail.com>
2025-10-01 14:37:55 +0200

    click like, dislike

--- src/Rule.jsx
+++ src/Rule.jsx
@@ -1,8 +1,6 @@
 // NPM dependencies
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faChevronDown, faPencil, faThumbsDown, faThumbsUp,
-} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronDown, faPencil, faThumbsDown, faThumbsUp,} from '@fortawesome/free-solid-svg-icons';
 import {useState} from "react";
 import {faChevronUp} from "@fortawesome/free-solid-svg-icons/faChevronUp";
 
@@ -11,13 +9,23 @@ import {faChevronUp} from "@fortawesome/free-solid-svg-icons/faChevronUp";
  */
 function Rule({ rule }) {
     const [folded , setFolded] = useState(false);
-    const clickHAndler = () => {
+    const [likeCount , setLikeCount] = useState(0);
+    const [dislikeCount , setDislikeCount] = useState(0);
+    const foldClicker = () => {
         setFolded(!folded)
+    }
+
+    const likeClicker = () => {
+        setLikeCount(likeCount+1);
+    }
+
+    const dislikeClicker = () => {
+        setDislikeCount(dislikeCount+1);
     }
 
     return (
         <section className="m-16 mt-4 cursor-pointer">
-            <header className="bg-blue-400 text-3xl p-4 text-white flex justify-between items-center" onClick={clickHAndler}>
+            <header className="bg-blue-400 text-3xl p-4 text-white flex justify-between items-center" onClick={foldClicker}>
                 {rule.title} <FontAwesomeIcon icon={folded ? faChevronUp: faChevronDown} className="m-4" />
             </header>
             <p className="p-4 text-2xl" hidden = {folded} >{rule.description}</p>
@@ -32,11 +40,11 @@ function Rule({ rule }) {
                         <FontAwesomeIcon icon={faPencil} />
                     </button>
                     <div className="flex">
-                        <button type="button" className="border p-4" title="+1">
-                            {rule.likes} <FontAwesomeIcon icon={faThumbsUp} />
+                        <button type="button" className="border p-4" title="+1" onClick={likeClicker}>
+                            {likeCount} <FontAwesomeIcon icon={faThumbsUp} />
                         </button>
-                        <button type="button" className="border p-4" title="+1">
-                            {rule.dislikes} <FontAwesomeIcon icon={faThumbsDown} />
+                        <button type="button" className="border p-4" title="+1" onClick={dislikeClicker}>
+                            {dislikeCount} <FontAwesomeIcon icon={faThumbsDown} />
                         </button>
                     </div>
                 </div>
